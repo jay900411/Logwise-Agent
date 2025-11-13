e9036e99cd384f9eae7cdb20fe79dab3da8ab405
jay900411 <119254312+jay900411@users.noreply.github.com>
2025-11-13 23:50:16 +0800

    Delete ollama_logwise directory

--- ollama_logwise/cli.py
+++
@@ -1,5 +0,0 @@
-# cli.py
-from .logwise_cli import main 
-
-if __name__ == "__main__":
-    main()
--- ollama_logwise/test/import_error.py
+++
@@ -1,9 +0,0 @@
-# import_error.py
-print("Attempting to import modules...")
-import os
-import sys
-
-# This module almost certainly does not exist
-import no_such_module_exists_12345
-
-print("Import successful. (This will not be printed)")
